3bd7b652fdb32780f3abba312f87a159f94f5816
GalaIO <505421435@qq.com>
2016-01-04 19:32:27 +0800

    电子天平+摄像头api

---
+++ cameraTest/cameraTest/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace cameraTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoPlayer = new AForge.Controls.VideoSourcePlayer();
+            this.list_cam = new System.Windows.Forms.ComboBox();
+            this.list_com = new System.Windows.Forms.ComboBox();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.button_cam_open = new System.Windows.Forms.Button();
+            this.button_cam_close = new System.Windows.Forms.Button();
+            this.button_com_open = new System.Windows.Forms.Button();
+            this.button_com_close = new System.Windows.Forms.Button();
+            this.text_info = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // videoPlayer
+            // 
+            this.videoPlayer.Location = new System.Drawing.Point(161, 12);
+            this.videoPlayer.Name = "videoPlayer";
+            this.videoPlayer.Size = new System.Drawing.Size(292, 237);
+            this.videoPlayer.TabIndex = 0;
+            this.videoPlayer.Text = "videoSourcePlayer1";
+            this.videoPlayer.VideoSource = null;
+            // 
+            // list_cam
+            // 
+            this.list_cam.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.list_cam.FormattingEnabled = true;
+            this.list_cam.Location = new System.Drawing.Point(24, 89);
+            this.list_cam.Name = "list_cam";
+            this.list_cam.Size = new System.Drawing.Size(121, 20);
+            this.list_cam.TabIndex = 1;
+            // 
+            // list_com
+            // 
+            this.list_com.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.list_com.FormattingEnabled = true;
+            this.list_com.Location = new System.Drawing.Point(24, 34);
+            this.list_com.Name = "list_com";
+            this.list_com.Size = new System.Drawing.Size(121, 20);
+            this.list_com.TabIndex = 2;
+            // 
+            // button_cam_open
+            // 
+            this.button_cam_open.Location = new System.Drawing.Point(24, 115);
+            this.button_cam_open.Name = "button_cam_open";
+            this.button_cam_open.Size = new System.Drawing.Size(50, 23);
+            this.button_cam_open.TabIndex = 3;
+            this.button_cam_open.Text = "打开";
+            this.button_cam_open.UseVisualStyleBackColor = true;
+            this.button_cam_open.Click += new System.EventHandler(this.button_cam_open_Click);
+            // 
+            // button_cam_close
+            // 
+            this.button_cam_close.Location = new System.Drawing.Point(95, 115);
+            this.button_cam_close.Name = "button_cam_close";
+            this.button_cam_close.Size = new System.Drawing.Size(50, 23);
+            this.button_cam_close.TabIndex = 4;
+            this.button_cam_close.Text = "关闭";
+            this.button_cam_close.UseVisualStyleBackColor = true;
+            this.button_cam_close.Click += new System.EventHandler(this.button_cam_close_Click);
+            // 
+            // button_com_open
+            // 
+            this.button_com_open.Location = new System.Drawing.Point(24, 60);
+            this.button_com_open.Name = "button_com_open";
+            this.button_com_open.Size = new System.Drawing.Size(50, 23);
+            this.button_com_open.TabIndex = 5;
+            this.button_com_open.Text = "打开";
+            this.button_com_open.UseVisualStyleBackColor = true;
+            this.button_com_open.Click += new System.EventHandler(this.button_com_open_Click);
+            // 
+            // button_com_close
+            // 
+            this.button_com_close.Location = new System.Drawing.Point(95, 60);
+            this.button_com_close.Name = "button_com_close";
+            this.button_com_close.Size = new System.Drawing.Size(50, 23);
+            this.button_com_close.TabIndex = 6;
+            this.button_com_close.Text = "关闭";
+            this.button_com_close.UseVisualStyleBackColor = true;
+            this.button_com_close.Click += new System.EventHandler(this.button_com_close_Click);
+            // 
+            // text_info
+            // 
+            this.text_info.Location = new System.Drawing.Point(24, 162);
+            this.text_info.Multiline = true;
+            this.text_info.Name = "text_info";
+            this.text_info.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.text_info.Size = new System.Drawing.Size(121, 87);
+            this.text_info.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 261);
+            this.Controls.Add(this.text_info);
+            this.Controls.Add(this.button_com_close);
+            this.Controls.Add(this.button_com_open);
+            this.Controls.Add(this.button_cam_close);
+            this.Controls.Add(this.button_cam_open);
+            this.Controls.Add(this.list_com);
+            this.Controls.Add(this.list_cam);
+            this.Controls.Add(this.videoPlayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoPlayer;
+        private System.Windows.Forms.ComboBox list_cam;
+        private System.Windows.Forms.ComboBox list_com;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button button_cam_open;
+        private System.Windows.Forms.Button button_cam_close;
+        private System.Windows.Forms.Button button_com_open;
+        private System.Windows.Forms.Button button_com_close;
+        private System.Windows.Forms.TextBox text_info;
+
+
+    }
+}
+
---
+++ cameraTest/cameraTest/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+
+using System.Drawing.Imaging;
+
+
+namespace cameraTest
+{
+    public partial class Form1 : Form
+    {
+        //摄像头设备 枚举
+        private FilterInfoCollection videoDevices;
+
+        public Form1()
+        {
+            InitializeComponent(); 
+            // 允许跨线程更新窗口控件
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //串口初始化
+            serialPortInnitial();
+            //摄像头初始化
+            cameraDevInnitial();
+        }
+
+        //串口初始化
+        private bool serialPortInnitial()
+        {
+            //搜索当前所有的com口，并列出来
+            string[] portName = SerialPort.GetPortNames();
+            if (portName.Length == 0)
+            {
+                this.list_com.Items.Add("unavaliable");
+            }
+            else
+            {
+                for (int i = 0; i < portName.Length; i++)
+                {
+                    this.list_com.Items.Add(portName[i]);
+                }
+            }
+            //显示默认项
+            this.list_com.SelectedIndex = 0;
+
+            //实例化串口对象
+            serialPort = new SerialPort();
+            //添加串口数据读取
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(comDataReceived);
+
+            return true;
+        }
+        //摄像头初始化
+        private bool cameraDevInnitial()
+        {
+            //搜索当前所有的视频设备，并列出来
+            try
+            {
+                // 枚举所有视频输入设备
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                foreach (FilterInfo device in videoDevices)
+                {
+                    this.list_cam.Items.Add(device.Name);
+                }
+
+                this.list_cam.SelectedIndex = 0;
+            }
+            catch (ApplicationException)
+            {
+                this.list_cam.Items.Add("unavaliable");
+                this.list_cam.SelectedIndex = 0;
+            }
+
+            return true;
+        }
+        private void CameraConn()
+        {
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[list_cam.SelectedIndex].MonikerString);
+            videoSource.DesiredFrameSize = new Size(this.videoPlayer.Width, this.videoPlayer.Height);
+            videoSource.DesiredFrameRate = 1;
+
+            videoPlayer.VideoSource = videoSource;
+            videoPlayer.Start();
+        }
+
+        private void button_cam_open_Click(object sender, EventArgs e)
+        {
+            CameraConn();
+        }
+
+        private void button_cam_close_Click(object sender, EventArgs e)
+        {
+            videoPlayer.SignalToStop();
+            videoPlayer.WaitForStop();
+        }
+        //拍照
+        private void photoShot(string pName, int width, int height)
+        {
+            try
+            {
+                if (videoPlayer.IsRunning)
+                {
+                    //获取当前视频帧
+                    Bitmap bitMap = new Bitmap(videoPlayer.GetCurrentVideoFrame(), width, height);
+                    bitMap.Save("./save/"+pName+".jpg",ImageFormat.Jpeg);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("摄像头异常：" + ex.Message);
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            button_cam_close_Click(null, null);
+            button_com_close_Click(null, null);
+        }
+
+        private void button_com_open_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(list_com.Items[list_com.SelectedIndex].ToString());
+            comOpen(list_com.Items[list_com.SelectedIndex].ToString(), 9600, Parity.None, 8, StopBits.One);
+
+        }
+
+        private void button_com_close_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();
+        }
+
+        private void comOpen(string comName, int baudRate, Parity parity, int DataBits, StopBits stopBits)
+        {
+            serialPort.PortName = comName;
+            serialPort.BaudRate = baudRate;
+            serialPort.DataBits = DataBits;
+            serialPort.Parity = parity;
+            serialPort.StopBits = stopBits;
+            serialPort.ReadTimeout = 500;
+
+            serialPort.Open();
+        }
+        //接收数据
+        private void comDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // 判断用户用的是字节模式还是字符模式
+            /*if (CurrentDataMode == DataMode.Text)
+            {
+                // 读取缓冲区的数据
+                string data = serialPort.ReadExisting();
+            }
+
+            else
+            {*/
+                // 获取字节长度
+                int bytes = serialPort.BytesToRead;
+
+                // 创建字节数组
+                byte[] buffer = new byte[bytes];
+
+                // 读取缓冲区的数据到数组
+                serialPort.Read(buffer, 0, bytes);
+
+                text_info.AppendText(Encoding.Default.GetString(buffer));
+                //photoShot("tmp",300,300);
+            //}
+
+        }
+    }
+}
